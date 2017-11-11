d4c381d50ccf7b2cf68d7fb38a599b16875b4343
Dimitar Dimitrov <dimitar.dimitrov@student.manchester.ac.uk>
2017-11-11 23:36:41 +0000

    added the ability to save the classfier object as a string and then recover it from it

--- SVM.py
+++ SVM.py
@@ -1,4 +1,5 @@
 from sklearn.svm import LinearSVC
+import pickle
 
 #returns a linear SVM classifier
 #see default values used for the model at
@@ -14,3 +15,13 @@ def train_clf(clf, feature_values, original_labels):
 #takes a classifier, the data; returns the guessed labels
 def classify(clf, feature_values):
     return clf.predict(feature_values)
+
+#returns a string that can be used with SVM.get_classffier_object(s)
+#to reconstruct the same object from the string
+#the string can be stored on disk and the training won't be lost
+def get_classifier_string(clf):
+    return pickle.dumps(clf)
+
+#returns an object from the saved classfier string
+def get_classffier_object(string):
+    return pickle.loads(string)
